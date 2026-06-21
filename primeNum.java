b57743cba6b862c368cde90139fd906b87944e70
Vishaal <vishaal21082004@gmail.com>
2026-06-21 17:54:24 +0530

    prime number w/o 3rd variable

---
+++ primeNum.java
@@ -0,0 +1,12 @@
+public class primeNum {
+    public static boolean isPrime(int n){
+        for(int i=2;i<n;i++){
+            if(n%i==0)
+                return false;
+        }
+        return true;
+    }
+    public static void main(String[] args) {
+        System.out.println(isPrime(13));
+    }
+}
